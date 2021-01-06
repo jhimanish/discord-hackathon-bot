cedee66a5c9e557f3e3f381987124d283c366bb6
unknown <jhimanish@gmail.com>
2021-01-05 19:29:54 -0500

    No need for extra txt files now

--- bot.py
+++ bot.py
@@ -30,66 +30,57 @@ async def verify(ctx, email_address):
     with open("participants.txt") as f:
         participants_arr = f.read().splitlines()
 
-    ieee_list_arr_used = []
-    with open("ieeeused.txt") as f:
-        ieee_list_arr_used = f.read().splitlines()
-
-    participants_arr_used = []
-    with open("participantsused.txt") as f:
-        participants_arr_used = f.read().splitlines()
-
     # checking for ieee email address
     for i in ieee_list_arr:
         j = i.split()
         if j[0].lower() == email_address.lower():
-            # moving to used list
-            ieee_list_arr.remove(i)
-            ieee_list_arr_used.append(i)
-            await ctx.send("You have been verified", delete_after=3.0)
-            # perms
-            await ctx.author.edit(nick=j[-1] + " (" + " ".join(j[1:-1]) + ")")
-            role = discord.utils.get(ctx.guild.roles, name="IEEE")
-            await ctx.author.add_roles(role)
-            role = discord.utils.get(ctx.guild.roles, name=" ".join(j[1:-1]))
-            await ctx.author.add_roles(role)
+            # check if user already exists
+            members = ctx.guild.members
+            found = False
+            for i in members:
+                if i.nick == (j[-1] + " (" + " ".join(j[1:-1]) + ")"):
+                    found = True
+            if found == False:
+                # send message about being verified
+                await ctx.send("You have been verified", delete_after=3.0)
+                # perms
+                await ctx.author.edit(nick=j[-1] + " (" + " ".join(j[1:-1]) + ")")
+                role = discord.utils.get(ctx.guild.roles, name="IEEE")
+                await ctx.author.add_roles(role)
+                role = discord.utils.get(ctx.guild.roles, name=" ".join(j[1:-1]))
+                await ctx.author.add_roles(role)
+            if found == True:
+                await ctx.send(
+                    "This email has already been used. If this is an error, please contact @Himanish",
+                    delete_after=3.0,
+                )
             break
 
     # checking for participant email address
     for i in participants_arr:
         j = i.split()
         if j[0].lower() == email_address.lower():
-            # moving to used list
-            participants_arr.remove(i)
-            participants_arr_used.append(i)
-            await ctx.send("You have been verified", delete_after=3.0)
-            # perms
-            await ctx.author.edit(nick=" ".join(j[1:]))
-            role = discord.utils.get(ctx.guild.roles, name="Hacker")
-            await ctx.author.add_roles(role)
+            # check if user already exists
+            members = ctx.guild.members
+            found = False
+            for i in members:
+                if i.nick == (" ".join(j[1:])):
+                    found = True
+            if found == False:
+                # send message about being verified
+                await ctx.send("You have been verified", delete_after=3.0)
+                # perms
+                await ctx.author.edit(nick=" ".join(j[1:]))
+                role = discord.utils.get(ctx.guild.roles, name="Hacker")
+                await ctx.author.add_roles(role)
+            if found == True:
+                await ctx.send(
+                    "This email has already been used. If this is an error, please contact @Himanish",
+                    delete_after=3.0,
+                )
             break
 
-    # writing to files
-    with open("ieeemembers.txt", "w") as f:
-        for item in ieee_list_arr:
-            f.write("%s\n" % item)
-    f.close()
-
-    with open("participants.txt", "w") as f:
-        for item in participants_arr:
-            f.write("%s\n" % item)
-    f.close()
-
-    with open("ieeeused.txt", "w") as f:
-        for item in ieee_list_arr_used:
-            f.write("%s\n" % item)
-    f.close()
-
-    with open("participantsused.txt", "w") as f:
-        for item in participants_arr_used:
-            f.write("%s\n" % item)
-    f.close()
-
-    await ctx.message.delete(delay=2.0)
+    await ctx.message.delete(delay=3.0)
 
 
 bot.run(TOKEN)
